da19a22ff7b6d0218f36c84928c02974b42fdb03
alexandershearer <alex.shearer23@gmail.com>
2020-10-15 15:51:06 -0500

    Started working on the landing page

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def greeting_posts():
+
+
+    return render_template('home.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
