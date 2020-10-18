e0e8e435d181904a8c9f072ef6556a0c2daa81d4
alexandershearer <alex.shearer23@gmail.com>
2020-10-18 13:59:46 -0500

    Added the about route and the about html

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def greeting_posts():
-
+    #This is rendering the home html and also getting all the posts from the posts database to show on the home page.
     posts = mongo.db.posts.find()
 
     return render_template('home.html', posts=posts)
 
-@app.route('/new_post', methods=['GET', 'POST'])
+@app.route('/new_post', methods=['GET', 'POST']) #This function is going to be used for the user to make a new post and save it into the database
 def create():
     if request.method == 'POST':
         post_title = request.form.get('post_title')
         
+        #This is getting the data from the form to store into the collection for posts
         new_post = {
             'title': post_title,
             'post_description': request.form.get('post_description'),
@@ -33,6 +34,10 @@ def create():
 
     else:
         return render_template('new_post.html') 
+
+@app.route('/about') #This function is going to render the about.html file 
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
